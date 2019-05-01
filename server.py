5d5331c9f9e50bc4dd84f67220abaff7065e100f
Aditya Kondapuram <kondapuram.aditya@gmail.com>
2019-05-01 09:54:25 -0300

    file name changed and server file is properly commented

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import socket, sys, xbox, time
-from multiprocessing import Process
 
 #port number is defined
 port = 5558
@@ -7,23 +6,27 @@ port = 5558
 #socket object is initialized
 def setupSocket():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #socket is set to reuse the current port
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print("Socket Successfully Created!")
 
     #socket connects the client(RPi) to the server
     try:
+        #socket binds to a ip addres which is automatically assigned from a client that is connecting
         s.bind(('', port))
         print("Socket has successfully binded to port ", port)
     except socket.error as msg:
         print(msg)
     return s
 
+#server socket listens for a connection 
 def setupConnection():
     s.listen(1)
     conn, addr = s.accept()
     print("Connected to ", addr[0], ":" + str(addr[1]))
     return conn
 
+#all button input is concatenated into a string (data)
 def buttonCollect(data):
     if joy.A():
         data += "AB1"
@@ -75,6 +78,7 @@ def buttonCollect(data):
         data += "RB0"
     return data
 
+#input from the sticks are concanenated to a string (data)
 def stickCollect(data):
     data += "LX" + str(joy.leftX())
     data += "LY" + str(joy.leftY())
@@ -82,13 +86,15 @@ def stickCollect(data):
     data += "RY" + str(joy.rightY())
     return data
 
+#input from triggers is concatenated to a string (data)
 def trigCollect(data):
     data += "LT" + str(joy.leftTrigger())
     data += "RT" + str(joy.rightTrigger())
+    #the END must be concatenated to break the string at the other end
     data += "END"
     return data
 
-#xbox joystick is set up here
+#xbox joystick class is initilized
 try:
     joy = xbox.Joystick()
     print("Joystick successfully initialized!")
@@ -96,38 +102,48 @@ except:
     print("Joystick could not initialize.")
     sys.exit()
 
-#actual start of the main program
-s = setupSocket()
-conn = setupConnection()
-
-while True:
-    data = ""
-    if joy.Start():
-        conn.send(str.encode("KILL"))
+#thisfunction checks to see if the controller is initiating shutdown
+def checkShutdown(connOrig):
+    #sends a KILL command to the client when pressing Start and Back together
+    if joy.Start() && joy.Back():
+        connOrig.send(str.encode("KILL"))
         time.sleep(1/2)
-        conn.close()
+        connOrig.close()
         print("Connection was manually severed. Hold Start to shut down server.")
         time.sleep(1)
-        if joy.Start():
+        #if Start and Back are held, the file shuts down too
+        if joy.Start() && joy.Back():
             joy.close()
             break
         else: 
-            time.sleep(1/2)
-            if joy.Start():
-                joy.close()
-                break
             conn = setupConnection()
-            
-    time.sleep(1/4)
+            return conn
+
+#program starts here
+s = setupSocket()
+conn = setupConnection()
+
+#main loop for the program which sends the string which contains all the controller input
+#to the client which must decode it properly
+while True:
+    data = ""
+    #program checks if shutdown by controller is initiated
+    conn = checkShutdown(conn)
+    
+    #string is concatenated into data
     data = buttonCollect(data)
     data = stickCollect(data)
     data = trigCollect(data)
+    
+    #sends HOLD command if controller is disconnected
     if not joy.connected():
         conn.sendall(str.encode("HOLD"))
+        time.sleep(5)
     else:
         conn.sendall(str.encode(data))
+    time.sleep(1/4)
 
+#everything is cleaned up after breaking the main loop
 s.close()
 print("Server is shutting down...")
-joy.close()
 sys.exit()
